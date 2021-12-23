b87af15d8522590594c65b66e56bb5a3d9fd4f7e
Pʀᴇᴅᴀᴛᴏʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-12-23 12:41:12 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -5,27 +5,26 @@ class Data:
     # Start Message
     START = """
 Hey {}
-
-Welcome to {}
+This is String Session Generator Bot
 
 If you don't trust this bot, 
-1) stop reading this message
-2) delete this chat
+1) Stop reading this message
+2) Delete this chat
 
 Still reading?
 You can use me to generate pyrogram and telethon string session. Use below buttons to learn more !
 
-By @StarkBots
+Made With ❤ By @TeleRoidGroup.
     """
 
     # Home Button
     home_buttons = [
         [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")],
-        [InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")]
+        [InlineKeyboardButton(text="🏡 Home ", callback_data="home")]
     ]
 
     generate_button = [
-        [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")]
+        [InlineKeyboardButton(" Start Generating Session", callback_data="generate")]
     ]
 
     # Rest Buttons
@@ -41,11 +40,10 @@ By @StarkBots
 
     # Help Message
     HELP = """
-✨ **Available Commands** ✨
-
+**How to Use the Bot and The Available Commands**
+start - Start the Bot
 /about - About The Bot
 /help - This Message
-/start - Start the Bot
 /generate - Start Generating Session
 /cancel - Cancel the process
 /restart - Cancel the process
@@ -53,15 +51,25 @@ By @StarkBots
 
     # About Message
     ABOUT = """
-**About This Bot** 
-
-A telegram bot to generate pyrogram and telethon string session by @StarkBots
-
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/StringSessionBot)
-
-Framework : [Pyrogram](docs.pyrogram.org)
-
-Language : [Python](www.python.org)
-
-Developer : @StarkProgrammer
+╭────[🔅Session Bot🔅]───⍟
+│
+├<b>🤖 Bot Name : <a href='https://t.me/TeleRoidSessionBot'>@TeleRoidSessionBot</a></b>
+│
+├<b>📢 Channel : <a href='https://t.me/TeleRoidGroup'>@TeleRoidGroup</a></b>
+│
+├<b>👥 Version : <a href='https://t.me/TeleRoidSessionBot'>0.9.2 beta</a></b>
+│
+├<b>💢 Source : <a href='https://github.com/PredatorHackerzZ'>Click Here</a></b>
+│
+├<b>🌐 Server : <a href='https://heroku.com'>Heroku</a></b>
+│
+├<b>📕 Library : <a href='https://github.com/pyrogram'>Pyrogram 1.2.8</a></b>
+│
+├<b>㊙ Language: <a href='https://www.python.org'>Python 3.9.4</a></b>
+│
+├<b>👨‍💻 Developer : <a href='https://t.me/PredatorHackerZ'>@Pred∆tor</a></b>
+│
+├<b>🚸 Powered By : <a href='https://t.me/Moviesflixers_DL'>@HindiWebNetwork</a></b>
+│
+╰──────[Thanks 😊]───⍟
     """
